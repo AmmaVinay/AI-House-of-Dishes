3e047fc3a308006a96392e07487fcc13dbbbe16a
AmmaVinay <ammavinay917@gmail.com>
2024-02-12 00:52:51 +0530

    Spanish Dishes Images

--- House-of-dishes-master/src/Data/SpainDishes/SpainBreakFast.js
+++ House-of-dishes-master/src/Data/SpainDishes/SpainBreakFast.js
@@ -1,28 +1,151 @@
-export default SpainBreakFast = [
-{
-dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-},
-{ dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-},
-{ dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-},
-{ dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-},
-{ dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-},
-{ dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-},
-{ dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-},
-{ dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886"
-}, { dishName: "Spaghetti Bolognese" ,
-dishImage: "https://c.ndtvimg.com/2021-04/umk8i7ko_pasta_625x300_01_April_21.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=886" },
-]
+const SpainBreakFast = [
+  {
+    dishName: "Spaghetti Bolognese",
+    dishImage: "https://live.staticflickr.com/4091/5023161954_6dfe5814df_b.jpg",
+  },
+  {
+    dishName: "Spanish Omelette",
+    dishImage: "https://example.com/spanish_omelette.jpg",
+  },
+  {
+    dishName: "Churros with Chocolate",
+    dishImage: "https://example.com/churros_with_chocolate.jpg",
+  },
+  {
+    dishName: "Pan con Tomate",
+    dishImage: "https://example.com/pan_con_tomate.jpg",
+  },
+  {
+    dishName: "Huevos Rotos",
+    dishImage: "https://example.com/huevos_rotos.jpg",
+  },
+  {
+    dishName: "Tortilla de Patatas",
+    dishImage: "https://example.com/tortilla_de_patatas.jpg",
+  },
+  { dishName: "Migas", dishImage: "https://example.com/migas.jpg" },
+  { dishName: "Ensaimada", dishImage: "https://example.com/ensaimada.jpg" },
+  { dishName: "Pisto", dishImage: "https://example.com/pisto.jpg" },
+  { dishName: "Morcilla", dishImage: "https://example.com/morcilla.jpg" },
+  { dishName: "Torrijas", dishImage: "https://example.com/torrijas.jpg" },
+  {
+    dishName: "Churro Ice Cream Sandwich",
+    dishImage: "https://example.com/churro_ice_cream_sandwich.jpg",
+  },
+  {
+    dishName: "Pulpo a la Gallega",
+    dishImage: "https://example.com/pulpo_a_la_gallega.jpg",
+  },
+  {
+    dishName: "Jamón Ibérico",
+    dishImage: "https://example.com/jamon_iberico.jpg",
+  },
+  { dishName: "Gazpacho", dishImage: "https://example.com/gazpacho.jpg" },
+  {
+    dishName: "Croissant with Jamón",
+    dishImage: "https://example.com/croissant_with_jamon.jpg",
+  },
+  { dishName: "Albondigas", dishImage: "https://example.com/albondigas.jpg" },
+  {
+    dishName: "Patatas Bravas",
+    dishImage: "https://example.com/patatas_bravas.jpg",
+  },
+  {
+    dishName: "Gambas al Ajillo",
+    dishImage: "https://example.com/gambas_al_ajillo.jpg",
+  },
+  {
+    dishName: "Calamares a la Romana",
+    dishImage: "https://example.com/calamares_a_la_romana.jpg",
+  },
+  {
+    dishName: "Fabada Asturiana",
+    dishImage: "https://example.com/fabada_asturiana.jpg",
+  },
+  { dishName: "Salmorejo", dishImage: "https://example.com/salmorejo.jpg" },
+  {
+    dishName: "Boquerones en Vinagre",
+    dishImage: "https://example.com/boquerones_en_vinagre.jpg",
+  },
+  {
+    dishName: "Pulpo a Feira",
+    dishImage: "https://example.com/pulpo_a_feira.jpg",
+  },
+  {
+    dishName: "Piquillos Rellenos",
+    dishImage: "https://example.com/piquillos_rellenos.jpg",
+  },
+  {
+    dishName: "Piquillos Rellenos de Marisco",
+    dishImage: "https://example.com/piquillos_rellenos_de_marisco.jpg",
+  },
+  {
+    dishName: "Piquillos Rellenos de Setas",
+    dishImage: "https://example.com/piquillos_rellenos_de_setas.jpg",
+  },
+  {
+    dishName: "Pimientos de Padrón",
+    dishImage: "https://example.com/pimientos_de_padron.jpg",
+  },
+  {
+    dishName: "Pimientos Rellenos de Carne",
+    dishImage: "https://example.com/pimientos_rellenos_de_carne.jpg",
+  },
+  {
+    dishName: "Pimientos Rellenos de Pescado",
+    dishImage: "https://example.com/pimientos_rellenos_de_pescado.jpg",
+  },
+  {
+    dishName: "Tigres (Stuffed Mussels)",
+    dishImage: "https://example.com/tigres.jpg",
+  },
+  { dishName: "Zarangollo", dishImage: "https://example.com/zarangollo.jpg" },
+  {
+    dishName: "Escudella i Carn d'Olla",
+    dishImage: "https://example.com/escudella_i_carn_dolla.jpg",
+  },
+  {
+    dishName: "Sepia a la Plancha",
+    dishImage: "https://example.com/sepia_a_la_plancha.jpg",
+  },
+  {
+    dishName: "Pulpo a la Brasa",
+    dishImage: "https://example.com/pulpo_a_la_brasa.jpg",
+  },
+  {
+    dishName: "Tarta de Santiago",
+    dishImage: "https://example.com/tarta_de_santiago.jpg",
+  },
+  {
+    dishName: "Arroz con Leche",
+    dishImage: "https://example.com/arroz_con_leche.jpg",
+  },
+  {
+    dishName: "Churro Bowl with Hot Chocolate",
+    dishImage: "https://example.com/churro_bowl_with_hot_chocolate.jpg",
+  },
+  { dishName: "Serranito", dishImage: "https://example.com/serranito.jpg" },
+  { dishName: "Gilda", dishImage: "https://example.com/gilda.jpg" },
+  {
+    dishName: "Pulpo a la Vinagreta",
+    dishImage: "https://example.com/pulpo_a_la_vinagreta.jpg",
+  },
+  {
+    dishName: "Crema Catalana",
+    dishImage: "https://example.com/crema_catalana.jpg",
+  },
+  { dishName: "Chistorra", dishImage: "https://example.com/chistorra.jpg" },
+  { dishName: "Txangurro", dishImage: "https://example.com/txangurro.jpg" },
+  {
+    dishName: "Bacalao a la Vizcaína",
+    dishImage: "https://example.com/bacalao_a_la_vizcaina.jpg",
+  },
+  {
+    dishName: "Pulpo a la Parrilla",
+    dishImage: "https://example.com/pulpo_a_la_parrilla.jpg",
+  },
+  {
+    dishName: "Tarta de Queso",
+    dishImage: "https://example.com/tarta_de_queso.jpg",
+  },
+];
