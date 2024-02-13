f818a91c016d636e07e6643119f4c19bf91e2823
AmmaVinay <ammavinay917@gmail.com>
2024-02-13 10:38:38 +0530

    Spain Dishes

--- House-of-dishes-master/src/Data/SpainDishes/SpainDinner.js
+++ House-of-dishes-master/src/Data/SpainDishes/SpainDinner.js
@@ -3,90 +3,127 @@
 export const SpainDinner = [
   {
     dishName: "Gazpacho",
-    dishImage: "https://example.com/gazpacho.jpg"
+    dishImage:
+      "https://shortgirltallorder.com/wp-content/uploads/2022/06/gazpacho-square-500x375.jpg"
   },
   {
     dishName: "Tapas Assortment",
-    dishImage: "https://example.com/tapas_assortment.jpg"
+    dishImage:
+      "https://api.openverse.engineering/v1/images/cd0e8bdb-ea65-44cb-b9e0-81206e40c15d/thumb/"
   },
   {
     dishName: "Jamón Ibérico",
-    dishImage: "https://example.com/jamon_iberico.jpg"
+    dishImage:
+      "https://api.openverse.engineering/v1/images/504b5271-0119-464e-9d03-e0b2757ff7f9/thumb/"
   },
   {
     dishName: "Salmorejo",
-    dishImage: "https://example.com/salmorejo.jpg"
+    dishImage:
+      "https://api.openverse.engineering/v1/images/8d4d64a6-680b-467e-b856-fdce73fcb5b3/thumb/"
   },
   {
     dishName: "Patatas Bravas",
-    dishImage: "https://example.com/patatas_bravas.jpg"
+    dishImage:
+      "https://www.recipesfromeurope.com/wp-content/uploads/2023/11/spanish-patatas-bravas-recipe-new-og-1.jpg"
   },
   {
     dishName: "Calamares a la Romana",
-    dishImage: "https://example.com/calamares_a_la_romana.jpg"
+    dishImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYEwZHdkfr8BBLt3M1fwmQP4qalRTmka4o2A&usqp=CAU"
   },
   {
     dishName: "Fabada Asturiana",
-    dishImage: "https://example.com/fabada_asturiana.jpg"
+    dishImage:
+      "https://api.openverse.engineering/v1/images/27fb5005-29fa-4859-8676-890fcabb646f/thumb/"
   },
   {
     dishName: "Cocido Madrileño",
-    dishImage: "https://example.com/cocido_madrileno.jpg"
+    dishImage:
+      "https://www.laespanolaaceites.com/wp-content/uploads/2019/06/cocido-madrileno-1080x671.jpg"
   },
   {
     dishName: "Arroz Negro",
-    dishImage: "https://example.com/arroz_negro.jpg"
+    dishImage:
+      "https://rightcasa.com/assets/components/phpthumbof/cache/arroz-negro-squid-rice.957ec626289cecc2a8395555931a2dc51979.jpg"
   },
   {
     dishName: "Bacalao a la Vizcaína",
-    dishImage: "https://example.com/bacalao_a_la_vizcaina.jpg"
+    dishImage:
+      "https://www.paulinacocina.net/wp-content/uploads/2023/05/bacalao-a-la-vizcaina-receta-facil.jpg"
   },
   {
     dishName: "Rabo de Toro",
-    dishImage: "https://example.com/rabo_de_toro.jpg"
+    dishImage:
+      "https://moorlandseater.com/wp-content/uploads/2018/02/rabo-de-toro-featured-OP-moorlands-eater-500x375.jpg"
   },
   {
     dishName: "Ensaladilla Rusa",
-    dishImage: "https://example.com/ensaladilla_rusa.jpg"
+    dishImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2ZSo6-WASKKYZgXCvYl_OWYuoqV9Y0y2FBw&usqp=CAU"
   },
   {
     dishName: "Cojonudos",
-    dishImage: "https://example.com/cojonudos.jpg"
+    dishImage:
+      "https://api.openverse.engineering/v1/images/7e9685fd-c7ab-4dfa-b640-8cca7f608dac/thumb/"
   },
   {
     dishName: "Espinacas con Garbanzos",
-    dishImage: "https://example.com/espinacas_con_garbanzos.jpg"
+    dishImage:
+      "https://letseattheworld.com/wp-content/uploads/2023/03/espinaca-garbonzas.jpg"
   },
   {
     dishName: "Cazon en Adobo",
-    dishImage: "https://example.com/cazon_en_adobo.jpg"
+    dishImage:
+      "https://content-cocina.lecturas.com/medio/2018/07/19/paso-a-paso-para-hacer-cazon-en-adobo-a-la-andaluza-resultado-final_2535cc1a_800x800.jpg"
   },
   {
     dishName: "Zarzuela de Mariscos",
-    dishImage: "https://example.com/zarzuela_de_mariscos.jpg"
+    dishImage:
+      "https://img-global.cpcdn.com/recipes/recipes_46955_v1393353833_receta_foto_00046955/400x400cq70/photo.jpg"
   },
   {
     dishName: "Cordero a la Miel",
-    dishImage: "https://example.com/cordero_a_la_miel.jpg"
+    dishImage:
+      "https://img-global.cpcdn.com/recipes/14aa4a8169dec296/1200x630cq70/photo.jpg"
   },
   {
     dishName: "Escudella i Carn d'Olla",
-    dishImage: "https://example.com/escudella_i_carn_dolla.jpg"
+    dishImage:
+      "https://images.hola.com/imagenes/cocina/recetas/20191210155961/escudella-carn-dolla-pilotas-nueces/0-757-219/escudella-card-olla-pilotes-nueces-t.jpg?tx=w_680"
   },
   {
     dishName: "Crema Catalana",
-    dishImage: "https://example.com/crema_catalana.jpg"
-  },
-  {
-    dishName: "Pisto",
-    dishImage: "https://example.com/pisto.jpg"
+    dishImage:
+      "https://spanishsabores.com/wp-content/uploads/2023/08/Crema-Catalana-Featured.jpg"
   },
   {
     dishName: "Sopa de Ajo",
-    dishImage: "https://example.com/sopa_de_ajo.jpg"
+    dishImage:
+      "https://recipes.net/wp-content/uploads/portal_files/recipes_net_posts/2021-08/spanish-garlic-soup-sopa-de-ajo-recipe.png"
   },
   {
     dishName: "Pulpo a la Plancha",
-    dishImage: "https://example.com/pulpo_a_la_plancha.jpg"
+    dishImage:
+      "https://img-global.cpcdn.com/recipes/bd50afdc022be00f/1200x630cq70/photo.jpg"
+  },
+  {
+    dishName: "Arroz a Banda",
+    dishImage:
+      "https://www.spain.info/export/sites/segtur/.content/imagenes/cabeceras-grandes/recetas/arroz-a-banda-t6300023.jpg_1014274486.jpg"
+  },
+  {
+    dishName: "Albondigas en Salsa",
+    dishImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSETc7egY2-82_O2RhAADwzxLrhBFPvFi5gbxbq1SaApM3XGroEPNj4BqvL1GAvDcoedJ4&usqp=CAU"
+  },
+  {
+    dishName: "Solomillo al Whisky",
+    dishImage:
+      "https://assets.tmecosys.cn/image/upload/t_web767x639/img/recipe/ras/Assets/9d5f0cc1b1d8b857c79e5b7127ee3e4a/Derivates/3d5d9771f567e731fecd0d3571bdad5bb5d8e058.jpg"
+  },
+  {
+    dishName: "Mariscada",
+    dishImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBq-JkE38LzxG_tgx-LzBsRKK_zSVoz0UC_g&usqp=CAU"
   }
 ];
